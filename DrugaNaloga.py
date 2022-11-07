aac324bc613d3dc97f5c15c0775733df320990ac
AMihevc <mihevc.anze@gmail.com>
2022-11-07 16:55:20 +0100

    Iskanje poti do slik in resnic

--- DrugaNaloga.py
+++ DrugaNaloga.py
@@ -1,1 +1,34 @@
+from math import cos, pi, sin
+import cv2
+import glob
+import numpy as np
+
+
 #Code for the solution of the assigment
+'''TODO
+implment yolo5
+    - odpreš vse slike z glob
+    - za vsako slikoo runaš model iz yolo 
+    - primerjaš z ground truth, ki je v .txt filih za vsako sliko
+
+implement Haar
+    - odpreš vsako lsiko z glob
+'''
+
+#Definicije funkcij
+
+#Funkcija najde vse slike in resnice in vrne seznam poti do slik in resnic
+def slike(pot):
+    poti_do_slik = glob.glob(pot + '*.png')
+    poti_do_resnic = glob.glob(pot + '*.txt')
+
+    return poti_do_slik, poti_do_resnic
+
+
+
+
+
+
+
+# ------------------------------------------ Začetek "izvajanja" programa --------------------------------------------------------------------------
+pot_do_testnih_podatkov = pot = "Support Files/ear_data/test/"
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+from math import cos, pi, sin
+import cv2
+import glob
+import numpy as np
+
+
 #random things I need to test before implementing in solution
+
+pot = "Support Files/ear_data/test/"
+absolutna_pot = "/home/anze/Documents/Faks/Slikovna biometrija/DrugaDomacaNaloga/sbNaloga2/Support Files/ear_data/test/"
+
+poti_do_slik = glob.glob(pot + '*.png')
+poti_do_resnic = glob.glob(pot + '*.txt')
+
+print(poti_do_resnic)
+
